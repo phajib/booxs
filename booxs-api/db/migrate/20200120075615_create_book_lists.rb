9c692d5fffa2e1a26260c863c0eb44251ba832fb
Haj B <phajib@gmail.com>
2020-01-20 00:27:19 -0800

    models integers fixed

--- booxs-api/db/migrate/20200120075615_create_book_lists.rb
+++ booxs-api/db/migrate/20200120075615_create_book_lists.rb
@@ -2,7 +2,7 @@ class CreateBookLists < ActiveRecord::Migration[6.0]
   def change
     create_table :book_lists do |t|
       t.integer :user_id
-      t.intger :book_id
+      t.integer :book_id
 
       t.timestamps
     end
