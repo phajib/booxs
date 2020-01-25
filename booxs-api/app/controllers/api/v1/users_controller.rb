28a7c0d4ef5615ed88403e9c131a057ac1ca6eb6
Haj B <phajib@gmail.com>
2020-01-25 15:54:47 -0800

    setting User

--- booxs-api/app/controllers/api/v1/users_controller.rb
+++ booxs-api/app/controllers/api/v1/users_controller.rb
@@ -3,4 +3,22 @@ class Api::V1::UsersController < ApplicationController
         @users = User.all
         render json: @users
     end
+
+    def new
+        @user = User.new
+        @book_list = BookList.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        @book_list = BookList.create(book_list_params)
+        if user.valid? 
+            session[:current_user_id] = user.id
+            session[:current_user_id] = book_list.id
+            render json: @user, except: [:created_at, :updated_at]
+            render json: @book_list, status 200
+        else
+            render :new
+        end
+    end
 end
