49c5df85510a07dedf54a6438c57f6cbfbbd1c36
Haj B <phajib@gmail.com>
2020-01-26 01:31:30 -0800

    book serializer added

--- booxs-api/app/controllers/api/v1/books_controller.rb
+++ booxs-api/app/controllers/api/v1/books_controller.rb
@@ -1,24 +1,23 @@
 class Api::V1::BooksController < ApplicationController
     def index
         @books = Book.all
-        render json: @books, status: 200
+        render json: BookSerializer.new(@books), status: 200
     end
 
     def show
         @book = Book.find(params[:id])
-        render json: @book, status: 200
+        render json: BookSerializer.new(@book), status: 200
     end
 
     def create
-        # @book = Book.new(book_params)
         @book = Book.create(book_params)
-        render json: @book, status: 200
+        render json: BookSerializer.new(@book), status: 200
     end
 
     def update
         @book = Book.find(params[:id])
         @book.update(book_params)
-        render json: @book, status: 200
+        render json: BookSerializer.new(@book), status: 200
     end
 
     def destroy
