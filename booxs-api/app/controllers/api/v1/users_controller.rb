86365cd0b239280d5150a753e0d9d619612c9857
Haj B <phajib@gmail.com>
2020-01-26 01:40:24 -0800

    implemented serializer

--- booxs-api/app/controllers/api/v1/users_controller.rb
+++ booxs-api/app/controllers/api/v1/users_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::UsersController < ApplicationController
     def index
         @users = User.all
-        render json: @users
+        render json: UserSerializer.new(@users)
     end
 
     def new
@@ -15,10 +15,24 @@ class Api::V1::UsersController < ApplicationController
         if user.valid? 
             session[:current_user_id] = user.id
             session[:current_user_id] = book_list.id
-            render json: @user, except: [:created_at, :updated_at]
-            render json: @book_list, status 200
+            render json: UserSerializer.new(@user)
+            render json: BookListSerializer,new(@book_list), status 200
         else
             render :new
         end
+    end
+
+    def show
+        @user = User.find_by(id: params[:id])
+        render json: UserSerializer.new(@user)
+    end
+
+    private 
+    def user_params
+        params.require(:user).permit(:email)
+    end
+    
+    def book_list_params
+        params.require(:book_list).permit(:user_id, :book_id)
     end
 end
