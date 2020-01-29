8c5e10b637afdbadd6b106c7835f2c1ce577b862
Haj B <phajib@gmail.com>
2020-01-29 03:47:05 -0800

    render

--- booxs-api/app/controllers/api/v1/books_controller.rb
+++ booxs-api/app/controllers/api/v1/books_controller.rb
@@ -1,43 +1,50 @@
 class Api::V1::BooksController < ApplicationController
     def index
         books = Book.all
+        # render json: books
+        options = { include: [:user] }
+        render json: BookSerializer.new(books, options)
         # render json: BookSerializer.new(books)
-        render json: books
     end
 
     def new
-        @book = book.new
+        @book = Book.new
     end
     
 
     def show
         book = Book.find(params[:id])
-        # render json: BookSerializer.new(book)
-        render json: book
+        render json: BookSerializer.new(book)
+        # render json: book
     end
 
     def create
-        @book = Book.create(book_params)
-        # render json: BookSerializer.new(@book), status: 200
-        render json: @book
+        book = Book.create(book_params)
+        if book
+            books = Book.all
+            options = { include: [:user] }
+            render json: BookSerializer.new(book, options)
+            # render json: Book.new(book), status: 200
+            # render json: book
+        end
     end
 
     def update
-        @book = Book.find(params[:id])
-        @book.update(book_params)
-        # render json: BookSerializer.new(@book), status: 200
-        render json: @book
+        book = Book.find(params[:id])
+        book.update(book_params)
+        render json: BookSerializer.new(book)
+        # render json: book
     end
 
     def destroy
-        @book = Book.find(params[:id])
-        @book.delete
+        book = Book.find(params[:id])
+        book.delete
 
-        render json: { book_id: @book.id }
+        render json: { book_id: book.id }
     end
 
     private
         def book_params
-            params.require(:book).permit(:id, :title, :author, :body_matter, :image, :user_id)
+            params.require(:book).permit(:id, :title, :author, :description, :image, :user_id)
         end
 end
--- booxs-api/app/controllers/api/v1/users_controller.rb
+++ booxs-api/app/controllers/api/v1/users_controller.rb
@@ -6,18 +6,14 @@ class Api::V1::UsersController < ApplicationController
 
     def new
         @user = User.new
-        # book_list = BookList.new
     end
 
     def create
         user = User.create(user_params)
-        book_list = BookList.create(book_list_params)
-        if user.valid? 
+        if user.valid?
             session[:current_user_id] = user.id
-            # session[:current_user_id] = book_list.id
-            render json: UserSerializer.new(user)
+            # render json: UserSerializer.new(user)
             render json: user, status: 200
-            # render json: BookListSerializer.new(book_list)
         else
             render :new
         end
@@ -30,10 +26,6 @@ class Api::V1::UsersController < ApplicationController
 
     private 
     def user_params
-        params.require(:user).permit(:email)
-    end
-    
-    def book_list_params
-        params.require(:book_list).permit(:user_id, :book_id)
+        params.require(:user).permit(:name, :email, :password)
     end
 end
