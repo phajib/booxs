9a8bf3fecacc0a426f7d503f37fe743e67bc0a30
Haj B <phajib@gmail.com>
2020-01-29 03:48:36 -0800

    index and current user set

--- booxs-api/app/controllers/application_controller.rb
+++ booxs-api/app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+    before_action :current_user
+
+  	def index
+  		render :login
+  	end
+
+  	def current_user
+  		@current_log_user ||= session[:current_user_id] &&
+      	User.find_by(id: session[:current_user_id])
+  	end
 end
